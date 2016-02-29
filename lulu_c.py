567f8ee03552e9a7f7ebb0ec747d3dd4c0cfdc45
Andrei Florea <andrei91ro@gmail.com>
2016-02-29 17:47:13 +0200

    successfully generating instance header

--- lulu_c.py
+++ lulu_c.py
@@ -2,14 +2,70 @@ import logging
 import colorlog # colors log output
 from lulu_pcol_sim import sim
 import sys # for argv
+import time # for strftime()
 
 def createInstanceHeader(pcol, path):
     """Create an instance of the passed P colony that is written as a header in C at the given path
 
     :pcol: The pcolony object that was read by lulu_pcol_sim
-    :path: The path to the instance.h that will be written
+    :path: The path to the instance.h that will be written"""
 
-    """
+    with open(path, "w") as fout:
+        fout.write("""// vim:filetype=c
+/**
+ * @file lulu_instance.h
+ * @brief Lulu P colony simulator internal structure.
+ * In this header we define the structure of the Pcolony that will power the simulated robot
+ * This file was generated automatically by lulu_c.py on %s
+ * @author Andrei G. Florea
+ * @author Catalin Buiu
+ * @date 2016-02-29
+ */
+#ifndef LULU_INSTANCE_H
+#define LULU_INSTANCE_H
+
+#include "lulu.h" """ % time.strftime("%d %h %Y at %H:%M"))
+
+        fout.write("\nenum objects {")
+        for i, obj in enumerate(pcol.A):
+            if (obj in ['e', 'f']):
+                continue; # they are already defined in lulu.h
+            if (i == 0):
+                # NO_OBJECT = 0, OBJECT_ID_E = 1, OBJECT_ID_F = 2
+                fout.write("\n    OBJECT_ID_%s = 3," % obj.upper());
+            else:
+                fout.write("\n    OBJECT_ID_%s," % obj.upper());
+
+        fout.write("\n};")
+
+        fout.write("\n\nenum agents {")
+        for i, agent_name in enumerate(pcol.B):
+            fout.write("\n    AGENT_%s," % agent_name.upper());
+
+        fout.write("\n};")
+        fout.write("""\n\n/**
+ * @brief Initialises the pcol object and all of it's components
+ *
+ * @param pcol The P colony that will be initialized
+ */
+void lulu_init(Pcolony_t *pcol);
+
+/**
+ * @brief Destroys the pcol objects and all of it's components
+ *
+ * @param pcol The P colony that will be destroyed
+ */
+void lulu_destroy(Pcolony_t *pcol);
+
+#endif""")
+# end createInstanceHeader()
+
+def createInstanceSource(pcol, path):
+    """Create an instance of the passed P colony that is written as a source file in C at the given path
+
+    :pcol: The pcolony object that was read by lulu_pcol_sim
+    :path: The path to the instance.c that will be written"""
+
     pass
 # end createInstanceHeader()
 
@@ -70,4 +126,7 @@ if (__name__ == "__main__"):
     else:
         pcol = pObj
 
-    createInstanceHeader(pcol, path)
+    logging.info("Generating the instance header (%s)" % (path + ".h"))
+    createInstanceHeader(pcol, path + ".h")
+    logging.info("Generating the instance source (%s)" % (path + ".c"))
+    createInstanceSource(pcol, path + ".c")
