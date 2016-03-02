5b7b0b19b39b8d9860e75ed0f8efff1c2ca8c016
Andrei Florea <andrei91ro@gmail.com>
2016-03-02 14:22:39 +0200

    succesfully generates instance.c

--- lulu_c.py
+++ lulu_c.py
@@ -66,7 +66,68 @@ def createInstanceSource(pcol, path):
     :pcol: The pcolony object that was read by lulu_pcol_sim
     :path: The path to the instance.c that will be written"""
 
-    pass
+    with open(path + ".c", "w") as fout:
+        fout.write("""#include "%s.h"
+#include <malloc.h>
+
+void lulu_init(Pcolony_t *pcol) {""" % path)
+
+        # call initPcolony()
+        fout.write("""\n    //init Pcolony with alphabet size = %d, nr of agents = %d, capacity = %d
+    initPcolony(pcol, %d, %d, %d);""" % (len(pcol.A), len(pcol.B), pcol.n,  len(pcol.A), len(pcol.B), pcol.n))
+
+        # init environment
+        fout.write("""\n\n    //init environment""")
+        counter = 0;
+        for obj, nr in pcol.env.items():
+            fout.write("""\n        pcol->env.items[%d].id = OBJECT_ID_%s;""" % (counter, obj.upper()))
+            fout.write("""\n        pcol->env.items[%d].nr = %d;\n""" % (counter, nr))
+            counter += 1
+        fout.write("""\n    //end init environment""")
+
+        fout.write("""\n\n    //init global pswarm environment""")
+        if (pcol.parentSwarm == None or len(pcol.parentSwarm.global_env) == 0):
+            fout.write("""\n        pcol->pswarm.global_env.items[0].id = OBJECT_ID_E;""")
+            fout.write("""\n        pcol->pswarm.global_env.items[0].nr = 1;""")
+        else:
+            counter = 0
+            for obj, nr in pcol.parentSwarm.global_env.items():
+                fout.write("""\n        pcol->pswarm.global_env.items[%d].id = OBJECT_ID_%s;""" % (counter, obj.upper()))
+                fout.write("""\n        pcol->pswarm.global_env.items[%d].nr = %d;""" % (counter, nr))
+            counter += 1
+        fout.write("""\n    //end init global pswarm environment""")
+
+        for ag_name in pcol.B:
+            fout.write("""\n\n    //init agent %s""" % ag_name)
+
+            fout.write("""\n        //init obj multiset""")
+            counter = 0;
+            for obj, nr in pcol.agents[ag_name].obj.items():
+                for i in range(nr):
+                    fout.write("""\n        pcol->agents[AGENT_%s].obj.items[%d] = OBJECT_ID_%s;""" % (ag_name.upper(), counter, obj.upper()))
+                    counter += 1
+
+            fout.write("""\n\n        //init programs""")
+            for prg_nr, prg in enumerate(pcol.agents[ag_name].programs):
+                fout.write("""\n\n            initProgram(&pcol->agents[AGENT_%s].programs[%d], pcol->n);""" % (ag_name.upper(), prg_nr))
+                fout.write("""\n            //init program %d: < %s >""" % (prg_nr, prg.print()))
+
+                for rule_nr, rule in enumerate(prg):
+                    fout.write("""\n                //init rule %d: %s""" % (rule_nr, rule.print(toString=True)) )
+                    if (rule.main_type != sim.RuleType.conditional):
+                        fout.write("""\n                initRule(&pcol->agents[AGENT_%s].programs[%d].rules[%d], RULE_TYPE_%s, OBJECT_ID_%s, OBJECT_ID_%s, NO_OBJECT, NO_OBJECT);""" % (ag_name.upper(), prg_nr, rule_nr, rule.type.name.upper(), rule.lhs.upper(), rule.rhs.upper()))
+                    else:
+                        fout.write("""\n                initRule(&pcol->agents[AGENT_%s].programs[%d].rules[%d], RULE_TYPE_CONDITIONAL_%s_%s, OBJECT_ID_%s, OBJECT_ID_%s, OBJECT_ID_%s, OBJECT_ID_%s);""" % (ag_name.upper(), prg_nr, rule_nr, rule.type.name.upper(), rule.alt_type.name.upper(), rule.lhs.upper(), rule.rhs.upper(), rule.alt_lhs.upper(), rule.alt_rhs.upper()))
+                fout.write("""\n            //end init program %d""" % prg_nr)
+            fout.write("""\n        //end init programs""")
+
+            fout.write("""\n    //end init agent %s""" % ag_name)
+
+        fout.write("""\n}""")
+        fout.write("""\n\nvoid lulu_destroy(Pcolony_t *pcol) {
+    //destroys all of the subcomponents
+    destroyPcolony(pcol);
+}""")
 # end createInstanceHeader()
 
 #   MAIN
@@ -129,4 +190,4 @@ if (__name__ == "__main__"):
     logging.info("Generating the instance header (%s)" % (path + ".h"))
     createInstanceHeader(pcol, path + ".h")
     logging.info("Generating the instance source (%s)" % (path + ".c"))
-    createInstanceSource(pcol, path + ".c")
+    createInstanceSource(pcol, path)
